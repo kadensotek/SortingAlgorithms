d721d4ce0303918ce2b24aa76a11c8e7505a5c08
kaden <kaden@enterprise.(none)>
2014-11-26 08:23:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Custom implementations of sorting algorithms
---
+++ src/SortDriver.java
@@ -0,0 +1,18 @@
+public class SortDriver
+{
+    public static void main(String[] args)
+    {
+        int[] array = new int[]{5, 3, 8, 1, 0, 2, 9, 7, 4, 6};
+        mainLoop(array);
+    }
+
+    public static void mainLoop(int[] array)
+    {
+        printMenu();
+    }
+
+    public static void printMenu()
+    {
+        System.out.println("Main Menu");
+    }
+}
