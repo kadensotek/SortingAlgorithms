c6f87be17b04fc613bd9c185e83f5d0de98594f6
kadensotek <rrj09832@gmail.com>
2014-11-27 12:30:28 -0500

    Added comments

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,5 +1,11 @@
 public class SelectionSort
 {
+    /*
+     *     Simple sort with complexity of O(n^2).
+     *     Small overhead so useful with small arrays.
+     *     Smallest amount of data movement of all sorts 
+     */
+
     public static <T extends Comparable<T>> void sort(T[] array)
     {
         for(int i=0; i<array.length; i++)
@@ -7,6 +13,7 @@ public class SelectionSort
             T minVal = array[i];
             int minValIndex = i;
 
+            /* Pass through rest of array and see if element is smaller than current min */
             for(int j=i+1; j<array.length; j++)
             {
                if(array[j].compareTo(minVal)<0)
@@ -16,6 +23,7 @@ public class SelectionSort
                }
             }
 
+            /* Swap elements */
             T temp = array[i];
             array[i] = array[minValIndex];
             array[minValIndex] = temp; 
