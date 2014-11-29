3d208018d03164fdd7ddcc3eb9d2069f0dd9e471
kadensotek <rrj09832@gmail.com>
2014-11-28 20:53:16 -0500

    Initialized MergeSort

---
+++ src/MergeSort.java
@@ -0,0 +1,15 @@
+public class MergeSort
+{
+    /*
+     *     Divide and conquer sort with complexity of O(n log n).
+     *     Uses recursive calls to sort array
+     *     Repeatedly divides array in half until every group has one or no elements
+     *     Then recombines in order to finish the sort
+     */
+
+    public static <T extends Comparable<T>> void sort(T[] array)
+    {
+        
+    }
+}
+
