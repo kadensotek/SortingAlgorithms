dd940f2eefaf459c4d7df6e107e3e050690b4df3
kadensotek <rrj09832@gmail.com>
2014-11-30 20:24:38 -0500

    Added some comments for clarity

--- src/linkedlist/BubbleSort.java
+++ src/linkedlist/BubbleSort.java
@@ -3,20 +3,30 @@ import java.util.LinkedList;
 
 public class BubbleSort
 {
+    /*
+     * Simple sort with complexity of O(n^2).
+     * Not useful outside of small arrays.
+     */
+
     public static <T extends Comparable<T>> void sort(List<T> list)
     {
         T first;
         T second;
         boolean hasChanged;
 
+       /* Cycles through N=array.length times to guarantee successful sort.
+        * Ends if a cycle results in no changes.
+        */
         for(int i=0; i<list.size()-1; i++)
         {
             hasChanged = false;
             for(int j=0; j<list.size()-1; j++)
             {
+                /* Two elements currently being compared */
                 first = list.get(j);
                 second = list.get(j+1);
 
+                /* swap elements if necessary */
                 if(first.compareTo(second) > 0)
                 {
                     swap(list, j, j+1); 
