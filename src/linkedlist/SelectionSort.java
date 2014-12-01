1c25e1525d44e05ea491436a4b787f2a58469753
kadensotek <rrj09832@gmail.com>
2014-11-30 22:42:33 -0500

    Initialized SelectionSort

---
+++ src/linkedlist/SelectionSort.java
@@ -0,0 +1,13 @@
+public class SelectionSort
+{
+    /*
+     *   Simple sort with complexity of O(n^2).
+     *   Small overhead so useful with small arrays.
+     *   Smallest amountof data movement of all sorts.
+     */
+
+    public static <T extends Comparable<T>> void sort(List<T> list)
+    {
+
+    }
+}
