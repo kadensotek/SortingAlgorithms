6f72a769e0913e9d6f8efb3df06187dc2cce965c
kadensotek <rrj09832@gmail.com>
2014-11-30 20:53:27 -0500

    Initialized InsertionSort

---
+++ src/linkedlist/InsertionSort.java
@@ -0,0 +1,13 @@
+public class InsertionSort
+{
+    /* 
+     *   Simple sort with complexity of O(n^2).
+     *   Small overhead so useful with small arrays.
+     *   Better than BubbleSort.
+     */
+
+    public static <T extends Comparable<T>> void sort(List<T> list)
+    {
+        
+    }
+}
