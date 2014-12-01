50e325e5f5e4868c4b8bd49eaa56299de24be9d7
kadensotek <rrj09832@gmail.com>
2014-12-01 10:27:31 -0500

    Initialized QuickSort

---
+++ src/linkedlist/QuickSort.java
@@ -0,0 +1,14 @@
+public class QuickSort
+{
+    /*
+    * Divide and conquer sort with complexity of O(n log n).
+    * Has worst case complexity of O(n^2).
+    * Uses recursive calls to sort array
+    * Considered the fastest sorting algorithm.
+    */
+
+    public static <T extends Comparable<T>> void sort(List<T> list)
+    {
+        
+    }
+}
